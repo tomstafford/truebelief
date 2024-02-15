32ed4a48f6d0a297505a84dddb19f55bc9f9901a
Tom Stafford <tom@idiolect.org.uk>
2024-02-15 21:54:57 +0000

    observational data!

---
+++ obs1.R
@@ -0,0 +1,121 @@
+#'''
+#load actual data and test
+#'''
+
+library(tidyverse)
+
+# get data
+
+
+
+dataloc <- "/home/tom/Downloads/indrostudy1data/study1_vaccine conspiracy_expressive responding_nums.csv"
+
+df <- read_csv(dataloc)
+
+
+# clean data
+
+df <- df %>%
+  filter(Progress==100) %>% 
+  filter(Status==0) %>%
+  filter(as.Date(EndDate) == as.Date("2022-08-02"))
+
+df <- df %>%
+  rename(Duration = `Duration (in seconds)`) %>%
+  mutate(Duration = readr::parse_integer(Duration))
+
+#find lower quartile of Duration
+lower_quartile <- quantile(df$Duration, probs = 0.25)
+
+df <- df %>% filter(Duration> lower_quartile*0.5)
+
+# attention check & other data quality measures??
+
+df <- df %>% 
+  filter(Vaccine_conspiracy_7=="2") %>%
+  filter(Vaccine_att_7=="5")
+
+# n = 500
+
+#drop att check columns
+
+df <- df %>%
+  select(!Vaccine_conspiracy_7) %>%
+  select(!Vaccine_att_7)
+
+# convert to integers
+
+df <- df %>%
+  mutate(across(Vaccine_conspiracy_1:Vaccine_conspiracy_8, as.integer)) %>%
+  mutate(across(Vaccine_att_1:Vaccine_att_11, as.integer)) %>%
+  mutate(across(Trust_NHS_1:Trust_UKGov_1, as.integer))
+
+df$Year_born <- as.integer(df$Year_born)
+
+# construct summary vars
+
+df <- df %>%
+  mutate(V = rowMeans(select(., starts_with("Vaccine_conspiracy_")), na.rm = TRUE)) %>%
+  mutate(T = rowMeans(select(., starts_with("Trust_")), na.rm = TRUE)) %>%
+  mutate(Vaccine_att_1 = 8 - Vaccine_att_1) %>% # reverse code so higher numbers are more skeptical attitudes
+  mutate(Vaccine_att_2 = 8 - Vaccine_att_2) %>%
+  mutate(C = rowMeans(select(., starts_with("Vaccine_att_")), na.rm = TRUE)) %>%
+  mutate(A = 2023 - Year_born)
+
+# sanity checks
+
+min(df$A)
+max(df$A)
+
+p <- ggplot(df,aes(x=A))
+p + geom_density()
+
+p <- ggplot(df,aes(x=V))
+p + geom_density()
+
+p <- ggplot(df,aes(x=T))
+p + geom_histogram()
+
+p <- ggplot(df,aes(x=C))
+p + geom_density()
+
+
+# tests
+# C  
+# Vaccine_conspiracy_1
+# Vaccine_conspiracy_7 #att check
+# Vaccine_conspiracy_8
+# 
+# A
+# Year_born
+# 
+# T
+# Trust_NHS_1
+# Trust_UKGov_1
+# 
+# V
+# Vaccine_att_1
+# Vaccine_att_2
+# Vaccine_att_3 4 5 6 8 9 10 11 #reverse
+# Vaccine_att_7 # att check
+
+# Without controlling for V
+
+m0 <- lm(data = df, C ~ A) # not controlling for V
+summary(m0)
+
+# A 0.005 (p = 0.023) - weak but significant
+
+m1 <- lm(data = df, C ~ A + V) # controlling for V
+summary(m1)
+
+# A -0.001 (p = 0.3)
+# V 0.39 (p<0.00001)
+
+m1 <- lm(data = df, C ~ A + T) # controlling for V
+summary(m1)
+
+# A 0.000 (p = 0.821)
+# T -0.351 (p<0.0001)
+
+write_csv(df %>% select(V,A,C,T),'obsdat.csv')
--- sim1.R
+++ sim1.R
@@ -1,10 +1,15 @@
 # '''
 # Trying to simulate data from a causal graph
 # 
-# V -> C
-# V <- C
-# A -> V
+# V : vaccine intention
+# I : conspiracy thinking
+# A : age
 # 
+# True Belief: A -> V <- C
+#          ie Vaccine Intention is a collider
+# Expressive responding: A -> V -> C 
+#          ie Vaccine Intention is a mediator
+
 # #install following https://github.com/RobinDenz1/simDAG
 # 
 # #first, at command line
@@ -15,6 +20,8 @@
 # remotes::install_github("RobinDenz1/simDAG")
 # '''
 
+# ------------- environment, parameters
+
 library(tidyverse)
 library(simDAG)
 library(gridExtra)
@@ -24,15 +31,18 @@ library(gridExtra)
 labssizerel=1.5
 
 modelname='Expressive responding'
+#modelname='True belief'
+
+# ------------- define two models
 
 if (modelname == 'Expressive responding'){
 
 #Expressive responding model
 dag <- empty_dag() +
   node("age", type="rnorm", mean=0, sd=1) +
-  node("int", type="gaussian", parents=c("age"), betas=c(1),
+  node("v_int", type="gaussian", parents=c("age"), betas=c(1),
        intercept=0, error=0.5) +
-  node("con", type="gaussian", parents=c("int"), betas=c(-1),
+  node("con", type="gaussian", parents=c("v_int"), betas=c(-1),
        intercept=0, error=0.5)
 
 } else { 
@@ -40,24 +50,28 @@ dag <- empty_dag() +
 modelname = 'True belief'
 dag <- empty_dag() +
   node("age", type="rnorm", mean=0, sd=1) +
-  node("int", type="gaussian", parents=c("age","con"), betas=c(1,-1),
+  node("v_int", type="gaussian", parents=c("age","con"), betas=c(1,-1),
        intercept=0, error=0.5) +
   node("con", type="rnorm", mean=0,sd =1)
 }
 
 
+# ------------------- simulate data
+
 sim_dat <- sim_from_dag(dag, n_sim=1000)
 
-p <- ggplot(data=sim_dat,aes(x=con,y=int))
+# -------------------- plot data
+
+p <- ggplot(data=sim_dat,aes(x=con,y=v_int))
 p1 <- p + geom_point(color='red')
 
-p <- ggplot(data=sim_dat,aes(x=con,y=int,color=age))
+p <- ggplot(data=sim_dat,aes(x=con,y=v_int,color=age))
 p2 <- p + geom_point() + labs(x = 'Conspiracy belief',y='Vaccine intention') +
   theme(axis.title.x = element_text(size = rel(labssizerel)),  # Increase the x-axis label font size
         axis.title.y = element_text(size = rel(labssizerel)))  # Increase the y-axis label font size
 
 
-p <- ggplot(data=sim_dat,aes(x=int,y=age))
+p <- ggplot(data=sim_dat,aes(x=v_int,y=age))
 p3 <- p + geom_point(color='green') + labs(x = 'Vaccine intention', y = 'Age') +
   theme(axis.title.x = element_text(size = rel(labssizerel)),  # Increase the x-axis label font size
         axis.title.y = element_text(size = rel(labssizerel)))  # Increase the y-axis label font size
@@ -77,4 +91,30 @@ plot_grid <- gridExtra::grid.arrange(p3, p4, p2, nrow = 1,
 savename <-  paste0(tolower(substr(modelname, 1, 4)), ".png")
 ggsave(savename,plot= plot_grid,dpi=120,width=15, height=5)
 
+
+# -------------------------- tests
+
+# predictions
+# # expressive responding: A & C correlate if no control for V, no correlate if do control
+# # true belief: A & C not correlated if no control for V, correlated if do control
+
 cor.test(sim_dat$age,sim_dat$con,method="pearson",conf.level=0.95)
+
+# Expressive responding : -0.81
+# True belief           : -0.01
+
+m0 <- lm(data = sim_dat, con ~ age) # not controlling for V
+summary(m0)
+
+# Expressive responding : age -0.99 (p<0.00001)
+# True belief           : age -0.01 (p = 0.67)
+
+m1 <- lm(data = sim_dat, con ~ age + v_int) # controlling for V
+summary(m1)
+
+# Expressive responding : age -0.07 (p=0.07)
+#                       v_int -0.94 (p<0.00001)
+
+# True belief           : age +0.82  (p<0.00001)
+#                       v_int -0.81  (p<0.00001)
+
